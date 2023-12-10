60b57a72282cfe16d85b264704e4d64d28bb9617
John Mettraux <jmettraux@gmail.com>
2023-12-10 18:45:16 +0900

    Draw a circle

--- try1/gen.rb
+++ try1/gen.rb
@@ -10,21 +10,23 @@ class Svg
   #
   # general svg
 
-#<svg width="506pt" height="332pt"
-# viewBox="0.00 0.00 506.00 332.00" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   def initialize(w, h, vb=nil, &block)
 
     @out = StringIO.new
 
     @out <<
-      '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' <<
+      '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' << "\n" <<
       '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" ' <<
       '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' <<
       "\n"
 
     vb = vb || "0 0 #{w.to_i} #{h.to_i}"
 
-    tag('svg', { width: w, height: h, viewBox: vb }, &block)
+    h = { width: w, height: h, viewBox: vb }
+    h['xmlns'] = "http://www.w3.org/2000/svg"
+    h['xmlns:xlink'] = "http://www.w3.org/1999/xlink"
+
+    tag('svg', h, &block)
   end
 
   def to_s
@@ -32,37 +34,50 @@ class Svg
     @out.string
   end
 
-  def g(attributes={}, &block)
+  def g(attributes={}, &block); tag('g', attributes, &block); end
+  def circle(attributes={}, &block); tag('circle', attributes, &block); end
+  def polygon(attributes={}, &block); tag('polygon', attributes, &block); end
+  def title(text, attributes={}); tag('title', attributes, text); end
 
-    tag('g', attributes, &block)
-  end
-
-  def tag(name, attributes, &block)
+  def tag(name, attributes, text=nil, &block)
 
     @out << '<' << name
     attributes.each do |k, v|
       @out << ' ' << k << '="' << v.to_s << '"' if v != nil
     end
-    @out << '>'
     if block
+      @out << '>'
       @out << "\n"
       instance_eval(&block)
       #@out << "\n"
+      @out << '</' << name << ">\n"
+    elsif text
+      @out << '>' << text << '</' << name << ">\n"
+    else
+      @out << "/>\n"
     end
-    @out << '</' << name << ">\n"
   end
 
   #
   # kabbatt
 
-  def ability(x, y, name, nam, &block)
+  #<circle cx="100" cy="100" r="50" fill="blue" />
+  #<polygon points="100,50 150,100 100,150 50,100" fill="red" />
+  def ability(x, y, name, nam)
 
-    g(id: nam, class: 'ability', &block)
+    r = 5
+    cx = x + r
+    cy = y + r
+
+    g(id: nam, class: 'ability') do
+      title(nam)
+      circle(cx: cx, cy: cy, r: r, fill: 'white', stroke: 'black')
+    end
   end
 end
 
 puts(
-  Svg.new('100pt', '200pt') do
-    ability(10, 10, 'Strength', 'STR')
+  Svg.new('300pt', '300pt') do
+    ability(100, 100, 'Strength', 'STR')
   end)
 
